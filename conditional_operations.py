b23cb7488635dd7226d2df40838f89727303b451
viemo777 <viemo777@gmail.com>
2022-10-11 16:33:07 +0300

    Initial commit

---
+++ conditional_operations.py
@@ -0,0 +1,38 @@
+x = int(input('input number: '))
+
+if x < 3:
+    print(str(x) +' < 3')
+elif x == 3:
+    print(str(x) +' == 3')
+else:
+    print(str(x) +' > 3')
+
+if 0:
+    print('0 is True')
+else:
+    print('0 is False')
+if 1:
+    print('1 or more is True')
+else:
+    print('1  is False')
+if "":
+    print('\'\' is True')
+else:
+    print('\'\' is False')
+if "wew3":
+    print('not empty string is True')
+else:
+    print('not empty string is False')
+if None:
+    print('None is True')
+else:
+    print('None is False')
+
+x = input('Enter any number')
+
+if x == '7':
+    print('7 is a lucky number! Today is your lucky day!')
+else:
+    print('Thank you! Have a nice day!')
+
+# False: 0, Empty string, None, empty object
